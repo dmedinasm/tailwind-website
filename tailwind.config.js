aba153ad67da2eea714d9d64f1453737ff3789c3
Daniel Miguel Medinas Mateo <dmedinasm@gmail.com>
2023-05-09 23:38:15 +0200

    Iniciando seccion de cabecera

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+  content: ["./src/*.{html,js}",
+            "./dist/*.{html, js}"
+],
+  theme: {
+    extend: {
+      colors: {
+        primary:'#0f172a',
+      },
+      fontFamily:{
+        roboto:['Roboto', ... defaultTheme.fontFamily.sans]
+      },
+    },
+  },
+  plugins: [],
+}
+
